45a848355ffd140f9cd9241be529f38c1fe696e3
黃仕勳 <shrshiunhuang@huangshixundeMacBook-Pro.local>
2023-03-18 12:54:06 +0800

    feat: set reward_type to parameter in training

--- AI_Trading/src/env_portfolio_allocation.py
+++ AI_Trading/src/env_portfolio_allocation.py
@@ -83,9 +83,11 @@ class portfolioAllocationEnv(gym.Env):
                 add_window = 0,
                 dis_bins = None,
                 dis_type = None,
-                cov = False):
+                cov = False,
+                reward_type = 'portfolioReturn'):
         self.dis_bins = dis_bins
         self.dis_type = dis_type
+        self.reward_type = reward_type
         self.cov = cov
         self.add_window = add_window
         self.day = self.add_window
@@ -169,71 +171,101 @@ class portfolioAllocationEnv(gym.Env):
             df = pd.DataFrame(self.portfolio_return_memory)
             df.columns = ['daily_return']
             print("=================================")
-            print("begin_total_asset:{}".format(self.asset_memory[0]))           
+            print("begin_total_asset:{}".format(self.asset_memory[0]))
             print("end_total_asset:{}".format(self.portfolio_value))
 
             df_daily_return = pd.DataFrame(self.portfolio_return_memory)
             df_daily_return.columns = ['daily_return']
 
             if df_daily_return['daily_return'].std() !=0:
-                sharpe = (252**0.5)*df_daily_return['daily_return'].mean()/ \
-                        df_daily_return['daily_return'].std()
-                print("Sharpe: ",sharpe)
+              sharpe = (252**0.5)*df_daily_return['daily_return'].mean()/ \
+                       df_daily_return['daily_return'].std()
+              print("Sharpe: ",sharpe)
             print("=================================")
             mdd = ep.max_drawdown(pd.Series(self.portfolio_return_memory))
             sortino = ep.sortino_ratio(pd.Series(self.portfolio_return_memory))
             calmar = ep.calmar_ratio(pd.Series(self.portfolio_return_memory))
-            reward = self.portfolio_value
+            reward = sum(self.reward_memory)
             if self.is_test_set == False:
-                df_training_weight = pd.DataFrame(self.actions_memory,columns =self.data.tic.values)
+                df_training_weight = pd.DataFrame(self.actions_memory,columns =self.data.loc[self.day,:].tic.values)
                 df_training_weight['date'] = self.date_memory
                 df_training_weight = df_training_weight.set_index('date')
                 df_training_weight.to_csv(self.training_weight_path)
+
                 if os.path.exists(self.training_log_path):
                     df_traing_log = pd.read_csv(self.training_log_path)
                     df_traing_log.loc[len(df_traing_log)] = [self.portfolio_value, reward, mdd, sharpe, sortino, calmar]
                     df_traing_log.to_csv(self.training_log_path, index= False)
                 else:
                     df_traing_log = pd.DataFrame({'portfolio value':[self.portfolio_value],
-                                                    'reward':[reward],
-                                                    'mdd': [mdd],
-                                                    'sharpe': [sharpe],
-                                                    'sortino': [sortino],
-                                                    'calmar':[calmar]})
+                                                  'reward':[reward],
+                                                  'mdd': [mdd],
+                                                  'sharpe': [sharpe],
+                                                  'sortino': [sortino],
+                                                  'calmar':[calmar]})
                     df_traing_log.to_csv(self.training_log_path, index= False)
 
             return self.state, self.reward, self.terminal, {}
         else:
+            # weights = torch.nn.Softmax(actions)
             weights = self.softmax_normalization(actions)
+            # weights = weights[:self.stock_dim]
             self.actions_memory.append(weights)
-            last_day_memory = self.data
-
+            last_day_memory = self.data.loc[self.day,:]
+            self.close_memory.append(last_day_memory.close.tolist())
             #load next state
             self.day += 1
-            self.data = self.df.loc[self.day,:]
-            self.covs = self.data['cov_list'].values[0]
-            self.state =  np.append(np.array(self.covs), [self.data[tech].values.tolist() for tech in self.tech_indicator_list ], axis=0)
+            self.data = self.df.loc[self.day-config.ADD_WINDOW:self.day]
+            if self.cov:
+                self.return_list = self.data.loc[self.day].return_list.to_list()[0]
+                self.covs = self.data['cov_list'].values[0]
+
+            info = []
+            if config.ADD_WINDOW > 0:
+                close_t = self.df.loc[self.day,:].close.to_list() #close_t
+                for i in range(config.ADD_WINDOW,-1,-1):
+                    info.append((self.df.loc[self.day-i].open/close_t).to_list())# open(closeNormalized)
+                    info.append((self.df.loc[self.day-i].high/close_t).to_list()) # high(closeNormalized)
+                    info.append((self.df.loc[self.day-i].low/close_t).to_list()) # low(closeNormalizedd)
+                    info.append((self.df.loc[self.day-i].close/close_t).to_list()) # close(closeNormalized)
+                    info.append(self.df.loc[self.day-i,:].macd.to_list()) # macd
+                    info.append(self.df.loc[self.day-i,:].macds.to_list()) # macds
+                    info.append(self.df.loc[self.day-i,:].macdh.to_list()) # macdh
+
+            self.state = info
+            # calcualte portfolio return
             share = np.floor(weights * self.portfolio_value / last_day_memory.close.values)
             self.share_memory.append(share)
             cash = self.portfolio_value - sum(share * last_day_memory.close.values)
 
             if self.transaction_cost_pct > 0:
-                share_change = np.sum(abs(np.array(share) - np.array(self.share_memory[self.day-1])) * np.array(last_day_memory.close.values))
-                print('share_change:',share_change)
+                share_change = np.sum(abs(np.array(share) - np.array(self.share_memory[self.day-config.ADD_WINDOW-1])) * np.array(last_day_memory.close.values))
                 trans_cost = share_change * self.transaction_cost_pct
             else:
                 trans_cost = 0
             # update portfolio value
-            new_portfolio_value = sum(share * self.data.close.values) + cash - trans_cost
-            self.portfolio_return = (new_portfolio_value / self.portfolio_value)-1
+            new_portfolio_value = sum(share * self.data.loc[self.day,:].close.values) + cash - trans_cost
+            self.portfolio_return = np.log(new_portfolio_value / self.portfolio_value)
             self.portfolio_value = new_portfolio_value
-
             # save into memory
             self.portfolio_return_memory.append(self.portfolio_return)
-            self.date_memory.append(self.data.date.unique()[0])            
+            self.date_memory.append(self.data.loc[self.day,:].date.unique()[0])
             self.asset_memory.append(new_portfolio_value)
 
-            self.reward = self.portfolio_return
+            var = np.var(self.portfolio_return_memory)
+            calmar = ep.calmar_ratio(pd.Series(self.portfolio_return_memory))
+            
+            if self.reward_type == 'portfolioReturn':
+                self.reward = self.portfolio_return # log-return
+            elif self.reward_type == 'riskConcern':
+               self.reward = (self.portfolio_return + self.alpha * var) # risk return 
+            elif self.reward_type == 'calmarConcern':
+                self.reward = (self.portfolio_return + abs(self.alpha * calmar)) # calmar return
+            elif self.reward_type == 'variance':
+                self.reward = abs(self.alpha * var) # variance
+            else:
+                print('no match reward')
+            self.reward_memory.append(self.reward)
         return self.state, self.reward, self.terminal, {}
 
     def reset(self):
---
+++ training.py
@@ -0,0 +1,101 @@
+import argparse
+from AI_Trading.src import config
+from AI_Trading.src.env_portfolio_allocation import *
+from AI_Trading.src import model_config
+from AI_Trading.src.preprocess import *
+from AI_Trading.src.testPortfolio import *
+from AI_Trading.src.train import *
+from AI_Trading.src.augmentation import *
+from stable_baselines3 import A2C, DDPG, PPO, SAC, TD3
+import pandas as pd
+from tqdm import tqdm
+from warnings import simplefilter
+simplefilter(action='ignore')
+
+def training(exp: str, reward: str, cov: bool, episode: int):
+    start = datetime.now()
+    create_dir()
+    model_name = 'DDPG'
+
+    # create training log folder
+    save_path = os.path.join(config.LOG_PATH, exp)
+    if not os.path.isdir(save_path):
+        try:
+            os.mkdir(save_path)
+        except Exception:
+            print(f'no folder {save_path}')
+            pass
+
+    # train
+    for i in tqdm(range(len(config.TRAIN_START_DATE))):
+        # create training log
+        training_log_path = f'{config.LOG_PATH}/{exp}/training_log_{model_name}_{i}.csv'
+        training_weight_path = f'{config.LOG_PATH}/{exp}/training_weight_{model_name}_{i}.csv'
+        training_share_path = f'{config.LOG_PATH}/{exp}/training_share_{model_name}_{i}.csv'
+        if os.path.exists(training_log_path):
+            os.remove(training_log_path)
+
+        if os.path.exists(training_weight_path):
+            os.remove(training_weight_path)
+
+        if os.path.exists(training_share_path):
+            os.remove(training_share_path)
+
+        train,trade = preprocess(config.TRAIN_START_DATE[0], config.TRAIN_END_DATE[i], config.TEST_START_DATE[i], config.TEST_END_DATE[i], window=config.ADD_WINDOW, cov=cov)
+        env_kwargs = {
+            "training_log_path": training_log_path,
+            "training_weight_path": training_weight_path,
+            "training_share_path": training_share_path,
+            "hmax": 100, 
+            "initial_amount": config.INITIAL_AMOUNT, 
+            "transaction_cost_pct": 0.001, 
+            "state_space": len(train.tic.unique())+4, 
+            "stock_dim": len(train.tic.unique()), 
+            "tech_indicator_list": config.INDICATORS, 
+            "action_space": len(train.tic.unique()), 
+            "reward_scaling": 1e-4,
+            "add_cash": False,
+            "alpha": config.REWARD_ALPHA,
+            "add_window": config.ADD_WINDOW,
+            "cov": cov,
+            "reward_type": reward
+        }
+        print(env_kwargs)
+        print('config.add_window:', config.ADD_WINDOW)
+
+        # env_train = blackLittermanEnv(df = train, is_test_set=False, **env_kwargs)
+        # env_trade = blackLittermanEnv(df = trade, is_test_set=True, **env_kwargs)
+        env_train = windowEnv(df = train, is_test_set=False, **env_kwargs)
+        env_trade = windowEnv(df = trade, is_test_set=True, **env_kwargs)
+
+        model_index = i
+        total_timesteps = len(train)/len(train.tic.unique())*episode
+        print('total timestep:', total_timesteps)
+        # load model
+        model_zip_path = f'{config.TRAINED_MODEL_PATH}/{exp}/{model_name}_{str(model_index-1)}.zip'
+        if os.path.exists(model_zip_path):
+            if model_name == 'A2C':
+                model = A2C.load(model_zip_path)
+            elif model_name == 'PPO':
+                model = PPO.load(model_zip_path)
+            elif model_name == 'DDPG':
+                model = DDPG.load(model_zip_path,env=env_train, seed=0,force_reset=True)
+            elif model_name == 'TD3':
+                model = TD3.load(model_zip_path)
+            elif model_name == 'SAC':
+                model = SAC.load(model_zip_path)
+            trainPortfolioAllocation(exp, env_train, model_name, model_index, continuous=True, model=model, total_timesteps=total_timesteps)
+        else:
+            trainPortfolioAllocation(exp, env_train, model_name, model_index, total_timesteps=total_timesteps)
+        end = datetime.now()
+        print(f'[{i}] model training time:{end-start}')
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-x', '--experiment', type=str, required=True)
+    parser.add_argument('-r', '--reward', type=str, default=80)
+    parser.add_argument('-c', '--cov', type=bool, default=False)
+    parser.add_argument('-e', '--episode', type=int, default=80)
+
+    args = parser.parse_args()
+    training(exp=args.experiment, reward= args.reward, cov=args.cov, episode=args.episode)
