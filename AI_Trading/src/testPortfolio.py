03fa28026302c9c34b271a49ccfa23708e01f037
黃仕勳 <shrshiunhuang@huangshixundeMacBook-Pro.local>
2023-07-19 16:42:24 +0800

    delete: delete useless file

--- AI_Trading/src/testPortfolio.py
+++
@@ -1,6 +0,0 @@
-from AI_Trading.src.stablebaselines3_models import DRLAgent as DRLAgent_sb3
-
-def test_portfolioAllocation(model, e_trade_gym):
-    df_daily_return,  df_actions = DRLAgent_sb3.DRL_prediction(model=model,
-                        environment = e_trade_gym)
-    return df_daily_return, df_actions
